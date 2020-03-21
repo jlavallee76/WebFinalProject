e28f0d3103fe697e1c2bafc673ed263ce45a70d4
Josh Lavallee <jlavallee76@academic.rrc.ca>
2020-03-21 12:35:04 -0500

    Finished Header/Nav & Footer
    A few things to tweak in footer (Adding links)

--- header.php
+++ header.php
@@ -15,9 +15,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora">
+    <link rel="stylesheet" href="assets/css/Article-Clean.css">
+    <link rel="stylesheet" href="assets/css/Article-Dual-Column.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Button.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Search.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/Footer-Dark.css">
+
   
     <!-- Custom CSS -->
     <link rel="stylesheet" href="sytles/mystyles.css">
   </head>
   <body>
+    <nav class="navbar navbar-dark navbar-expand-md sticky-top navigation-clean-button" style="background-color: rgba(0,0,0,0.83);">
+          <div class="container"><a class="navbar-brand" href="index.php" style="color: rgb(255,255,255);"><img data-bs-hover-animate="pulse" src="assets/img/logo.png" style="width: 121px;"></a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+              <div
+                  class="collapse navbar-collapse" id="navcol-1">
+                  <ul class="nav navbar-nav mr-auto">
+                      <li class="nav-item" role="presentation"></li>
+                      <li class="nav-item" role="presentation"><a class="nav-link" data-bs-hover-animate="pulse" href="episodes.php" style="color: rgb(255,255,255);">Episodes</a></li>
+                      <li class="nav-item" role="presentation"><a class="nav-link" data-bs-hover-animate="pulse" href="houseguests.php" style="color: rgb(255,255,255);">Houseguests</a></li>
+                      <li class="nav-item" role="presentation"><a class="nav-link" data-bs-hover-animate="pulse" href="fantasy.php" style="color: rgb(255,255,255);">Fantasy</a></li>
+                  </ul>
+                  <span class="navbar-text actions"> 
+                    <a class="btn btn-light action-button" role="button" data-bs-hover-animate="jello" href="signup.php" style="background-color: rgba(218,4,3,0);margin: 0px;margin-right: 14px;">Sign Up</a>
+                    <a class="btn btn-light action-button" role="button" data-bs-hover-animate="jello" href="login.php" style="background-color: rgb(218,4,3);">Log In</a>
+                  </span>             
+              </div>
+          </div>
+      </nav>
