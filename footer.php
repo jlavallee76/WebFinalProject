523ee8c8eea05bc1cc6f63566b8d8d2dec201301
Josh Lavallee <jlavallee76@academic.rrc.ca>
2020-03-21 14:12:09 -0500

    Removed/Update
    Removed un-necessary image files and cleaned up css for all pages.

--- footer.php
+++ footer.php
@@ -1,47 +1,39 @@
-<!---------------------------------------------------
-    Final Project: CMS - Big Brother Fan Discussion -
-    Name: Josh Lavallee                             -
-    Course: WEBD-2006 Section 1                     -
-    Date: March 20th 2020                           -
-    Description: Footer                             -
------------------------------------------------------>
-<div class="footer-dark" style="background-color: rgba(0,0,0,0.83);">
-  <footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-6 col-md-3 item">
-                <h3 style="color: rgb(218,4,3);">Citations</h3>
-                <ul>
-                    <li><a href="#">Big Brother Canada</a></li>
-                    <li><a href="#">Wikipedia</a></li>
-                    <li></li>
-                </ul>
+   <div class="footer-dark" style="background-color: rgba(0,0,0,0.83);">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3 style="color: rgb(218,4,3);">Citations</h3>
+                        <ul>
+                            <li><a href="#">Big Brother Canada</a></li>
+                            <li><a href="#">Wikipedia</a></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3 style="color: rgb(218,4,3);">About</h3>
+                        <ul>
+                            <li><a href="#">Episodes</a></li>
+                            <li><a href="#">Houseguests</a></li>
+                            <li><a href="#">Fantasy</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3 style="color: rgb(218,4,3);">Designer</h3>
+                        <p>Josh Lavallee is a Red River student enrolled in the Business Information Technology program in Winnipeg, Manitoba. More work can be found at yoshigrams.com and joshlavallee.dev.</p>
+                    </div>
+                    <div class="col item social"><a class="border rounded" href="#" style="background-color: rgb(218,4,3);"><i class="icon ion-social-facebook"></i></a><a class="border rounded" href="#" style="background-color: rgb(218,4,3);"><i class="icon ion-social-twitter"></i></a>
+                        <a
+                            class="border rounded" href="#" style="background-color: rgb(218,4,3);"><i class="icon ion-social-snapchat"></i></a><a class="border rounded" href="#" style="background-color: rgb(218,4,3);"><i class="icon ion-social-instagram"></i></a></div>
+                </div>
+                <p class="copyright">Josh Lavallee © 2020</p>
             </div>
-            <div class="col-sm-6 col-md-3 item">
-                <h3 style="color: rgb(218,4,3);">About</h3>
-                <ul>
-                    <li><a href="#">Episodes</a></li>
-                    <li><a href="#">Houseguests</a></li>
-                    <li><a href="#">Fantasy</a></li>
-                </ul>
-            </div>
-            <div class="col-md-6 item text">
-                <h3 style="color: rgb(218,4,3);">Designer</h3>
-                <p>Josh Lavallee is a Red River student enrolled in the Business Information Technology program in Winnipeg, Manitoba. More work can be found at yoshigrams.com and joshlavallee.dev.</p>
-            </div>
-            <div class="col item social"><a class="border rounded" href="#" style="background-color: rgb(218,4,3);"><i class="icon ion-social-facebook"></i></a><a class="border rounded" href="#" style="background-color: rgb(218,4,3);"><i class="icon ion-social-twitter"></i></a>
-                <a
-                    class="border rounded" href="#" style="background-color: rgb(218,4,3);"><i class="icon ion-social-snapchat"></i></a><a class="border rounded" href="#" style="background-color: rgb(218,4,3);"><i class="icon ion-social-instagram"></i></a></div>
-        </div>
-        <p class="copyright">Josh Lavallee © 2020</p>
+        </footer>
     </div>
-  </footer>
-</div>
-
-<!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
     <script src="assets/js/bs-init.js"></script>
-  </body>
+</body>
+
 </html>
--- index.php
+++ index.php
@@ -1,10 +1,3 @@
-<!---------------------------------------------------
-    Final Project: CMS - Big Brother Fan Discussion -
-    Name: Josh Lavallee                             -
-    Course: WEBD-2006 Section 1                     -
-    Date: March 20th 2020                           -                     
-    Description: Index Page                         -
------------------------------------------------------>
 <?PHP
     $title = 'Home';
     require_once 'header.php';
